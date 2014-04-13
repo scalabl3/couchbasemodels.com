ed8e4445d188e680b83e1fdbbb1cab474851fa3c
Jasdeep Jaitla <jasdeep@scalabl3.com>
2014-04-12 18:06:17 -0700

    daemonize the puma

--- config/puma.rb
+++ config/puma.rb
@@ -3,10 +3,15 @@ threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 12)
 
 preload_app!
 
-rackup      DefaultRackup
-#port        ENV['port_cbmodels'] || 3001
-bind "unix:///www/run/cbmodels.sock"
-environment ENV['env_cbmodels'] || 'production'
+rackup                DefaultRackup
+#port                 ENV['port_cbmodels'] || 3001
+daemonize             true
+bind                  "unix:///www/run/cbmodels.sock"
+activate_control_app  "unix:///www/run/cbmodelsctl.sock"
+pidfile               '/www/run/cbmodels.pid'
+state_path            "/www/run/cbmodels.state"
+stdout_redirect       "/www/log/cbmodels.stdout.log", "/www/log/cbmodels.stderr.log"
+environment           ENV['env_cbmodels'] || 'production'  
 
 on_worker_boot do
 
