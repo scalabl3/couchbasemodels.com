13fd88426d4e02ed576b9a4154ebb2a626ddc837
Jasdeep Jaitla <jasdeep@scalabl3.com>
2014-04-16 10:38:47 -0700

    update logging path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,10 @@ CouchbaseModels::Application.configure do
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
-
+	config.paths.log = "/www/log/cbmodels.rails.#{Rails.env}.log"
+	config.logger = Logger.new(config.paths.log)
+	config.logger.level = Logger::INFO
+	
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
 
